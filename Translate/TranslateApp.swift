2956db9d67c3bb2b089d5bfe384e2e14d044f3db
eyhdev <alaqsa335@gmail.com>
2023-08-06 15:22:57 +0300

    Initial Commit

---
+++ Translate/TranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateApp.swift
+//  Translate
+//
+//  Created by eyh.mac on 6.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
